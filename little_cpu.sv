17d5f5c2fdac5d43968d46dfb299ae32f690b775
hailey <_____@___.com>
2024-04-28 07:05:41 -0500

    got all the pins on all the chips

--- little_cpu.sv
+++ little_cpu.sv
@@ -1,37 +1,96 @@
-module little_cpu #(parameter BITS=16)(
+module little_cpu(
     input wire i_clk,
     input wire i_rst
 );
 
 
-control ctrl( 
-    .i_opcode(), //8bit
-    .i_clk(i_clk),
-    .i_acc_zero(),
-    .o_ld_mdr(),
-    .o_ld_mar(),
-    .o_ld_pc(),
-    .o_ld_acc(),
-    .o_alu_ctrl(),
-    .o_mux_ir_p1(),
-    .o_mux_mdr_alur(),
-    .o_mux_pc_ird()
+control ctrl(
+    i_opcode(), //[3:0]
+    i_clk(),
+    i_rst(),
+    i_acc_zero(),
+
+    o_ld_mdr(),
+    o_ld_mar(),
+    o_ld_pc(),
+    o_ld_acc(),
+    o_ld_ir(),
+    
+    /*on mux, 0 = first*/
+    o_mux_ir_p1(),
+    o_mux_mdr_alur(),
+    o_mux_pc_ird(),
+    o_alu_ctrl()
 );
 
-register #(BITS=16) inst_reg( );
+register #(16) inst_reg ( 
+    i_ld(),
+    i_data(),
+    o_data()
+);
 
-p1       pc_plus_one        ( );
+p1 pc_plus_one ( 
+    in(),
+    out()
+);
 
-mux_2    mx_p1_inst_rg      ( );
-register prog_ctr           ( );
+mux_2 mx_p1_inst_rg ( 
+    i_ab_sw(),
+    i_a(),
+    i_b(),
+    o_out()
+);
 
-mux_2    mx_ir_pc           ( );
-register mem_addr_reg       ( );
-memory   mem                ( );
-register mem_data_reg       ( );
+register #(8)prog_ctr (
+    i_ld(),
+    i_data(),
+    o_data()
+ );
 
-mux_2    mx_alu_ir          ( );
-alu      alu                ( );
-accum    accum              ( );
+mux_2 mx_ir_pc (
+    i_ab_sw(),
+    i_a(),
+    i_b(),
+    o_out()
+ );
+ 
+register mem_addr_reg ( 
+    i_ld(),
+    i_data(),
+    o_data()
+);
+memory mem ( 
+    i_rw(),
+    i_data(),
+    i_addr(),
+    o_data()
+);
+
+register mem_data_reg (
+    i_ld(),
+    i_data(),
+    o_data()
+ );
+
+mux_2 mx_alu_ir ( 
+    i_ab_sw(),
+    i_a(),
+    i_b(),
+    o_out()
+);
+
+alu alu ( 
+    i_op(),
+    i_l(),
+    i_r(),
+    o_out()
+);
+
+accum accum (
+    i_ld(),
+    i_data(),
+    o_data(),
+    o_zero()
+);
   
 endmodule;
--- memory.sv
+++ memory.sv
@@ -6,16 +6,16 @@ module memory #(parameter BITS=8, MEMADDRS=256)
     output wire [BITS-1:0]   o_data
 );
 
-reg [BITS-1:0] mem [0:MEMADDRS-1];
+reg [BITS-1:0] regmem [0:MEMADDRS-1];
 
 always_latch begin
     if(i_rw) begin
-        assign mem[i_addr] = i_data;    
+        assign regmem[i_addr] = i_data;    
     end
     
 end
 
 
-assign o_data = mem[i_addr];
+assign o_data = regmem[i_addr];
 
 endmodule
