86a769b1634d67211b3fc93485b669ac1915c5c8
hailey g <MY_NAME@example.com>
2024-04-28 19:06:39 -0500

    better scripts

--- tst.sh
+++ tst.sh
@@ -2,7 +2,16 @@
 set -e
 
 echo "### Verlating $1 ###"
-verilator --prof-cfuncs -CFLAGS -DVL_DEBUG --Wall --trace -cc $1.sv --exe tb_$1.cpp
+FILE_PATH=""
+if test -f $1.sv; then
+    FILE_PATH="$1.sv"
+fi
+
+if test -f $1.v; then
+    FILE_PATH="$1.v"
+fi
+
+verilator --prof-cfuncs -CFLAGS -DVL_DEBUG --Wall --trace -cc $FILE_PATH --exe tb_$1.cpp
 echo "### BUILDING $1 ###"
 make -C obj_dir -f V$1.mk V$1 -j 4
 echo "### SIMULATING $1 ###"
